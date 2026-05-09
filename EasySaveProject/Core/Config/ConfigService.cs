1e6bacb2def626e5c4c263439b0f82811fb9e36b
Dedale10 <romainhumbert010@gmail.com>
2026-05-09 13:49:44 +0200

    Ajout du système de choix du format des logs, ajout des traductions, modification interface

--- EasySaveProject/Core/Config/ConfigService.cs
+++ EasySaveProject/Core/Config/ConfigService.cs
@@ -1,35 +1,48 @@
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 public class ConfigService
 {
     private readonly string FilePath;
 
+    private static readonly JsonSerializerOptions _options = new JsonSerializerOptions
+    {
+        WriteIndented = true,
+        Converters = { new JsonStringEnumConverter() }
+    };
+
     public ConfigService()
     {
-        FilePath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory,
-        "..", "..", "..", "Data", "Config", "appsettings.json"));
+        FilePath = Path.GetFullPath(Path.Combine(
+            AppContext.BaseDirectory,
+            "..", "..", "..", "Data", "Config", "appsettings.json"
+        ));
     }
 
     public AppConfig Load()
     {
         if (!File.Exists(FilePath))
-        {
             return new AppConfig();
-        }
 
         var json = File.ReadAllText(FilePath);
 
         if (string.IsNullOrWhiteSpace(json))
+            return new AppConfig();
+
+        try
+        {
+            return JsonSerializer.Deserialize<AppConfig>(json, _options)
+                   ?? new AppConfig();
+        }
+        catch
         {
             return new AppConfig();
         }
-
-        return JsonSerializer.Deserialize<AppConfig>(json) ?? new AppConfig(); // "??" -> Null-coalescing operator : If the left side is null, use the right side instead
     }
 
     public void Save(AppConfig config)
     {
-        var json = JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true });
-        File.WriteAllText(FilePath, json);
+        Directory.CreateDirectory(Path.GetDirectoryName(FilePath)!);
+        File.WriteAllText(FilePath, JsonSerializer.Serialize(config, _options));
     }
 }
