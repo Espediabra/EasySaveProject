1fcd8f4ddb05a576020c44a93036ebe8d9d7953e
g1obu1eb1eu <147941528+g1obu1eb1eu@users.noreply.github.com>
2026-05-15 11:49:02 +0200

    Modification of .csproj & programm content.

--- EasySaveProject/Program.cs
+++ EasySaveProject/Program.cs
@@ -1,18 +1,43 @@
-using EasySaveProject.Helpers;
+// using EasySaveProject.Helpers;
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         var app = new App();
+
+//         if (args.Length == 0)
+//         {
+//             app.Run();
+//             // Console.WriteLine("Aucun args fournis");
+//             return;
+//         }
+//         Console.WriteLine($"Les args sont {string.Join(", ", args)}");
+//         app.RunCli(args);
+//     }
+// }
+
+using Avalonia;
+using System;
+
+namespace EasySaveProjectUI;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        var app = new App();
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
 
-        if (args.Length == 0)
-        {
-            app.Run();
-            // Console.WriteLine("Aucun args fournis");
-            return;
-        }
-        Console.WriteLine($"Les args sont {string.Join(", ", args)}");
-        app.RunCli(args);
-    }
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+#if DEBUG
+            .WithDeveloperTools()
+#endif
+            .WithInterFont()
+            .LogToTrace();
 }
